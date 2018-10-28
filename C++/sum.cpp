ce585326ec556d8ae4e6a66f5bb7e9bde5551f26
Ninad Madhab <madhab.ninad98@gmail.com>
2018-10-28 21:15:48 +0530

    Updated

---
+++ C++/sum.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float n1,n2;
+    cout<<"Enter first number\n";
+    cin>>n1;
+    cout<<"Enter second number\n";
+    cin>>n2;
+    float sum = n1+n2;
+    cout<<"The sum of "<<n1<<" and "<<n2<<" is "<<sum;
+}
