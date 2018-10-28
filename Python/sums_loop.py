69657cc180b1d48996201deeb4feeb8fc73eb24d
Ninad Madhab <madhab.ninad98@gmail.com>
2018-10-29 00:35:11 +0530

    Update

---
+++ Python/sums_loop.py
@@ -0,0 +1,14 @@
+num = int(input("Enter how many pairs to be added\n"))
+sum = [{0}]
+arr1 = []
+arr2 = []
+for i in range(num):
+    arr1.append(int(input()))
+    arr2.append(int(input()))
+print("The resluts are:\n")
+for x,y in zip(arr1,arr2):
+    print(x+y)
+    
+
+   
+    
